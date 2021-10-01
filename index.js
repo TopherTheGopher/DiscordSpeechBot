882f4adc485eb2fc846580aa09973b1a2755f328
Christopher Brennan <chrisleebrennan@gmail.com>
2021-10-01 03:05:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -916,8 +916,9 @@ async function transcribe_witai(buffer) {
         console.log(output)
         stream.destroy()
 
-	console.log(Object.keys(output).length)
-
+	for (const key in output) {
+		console.log(key)
+	}
 	    
         if (output && '_text' in output && output._text.length)
             return output._text
