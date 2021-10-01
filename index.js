291db3d24e9700c26487008b8ee5c3b7a4329e3f
Christopher Brennan <chrisleebrennan@gmail.com>
2021-09-30 20:11:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -915,10 +915,12 @@ async function transcribe_witai(buffer) {
         witAI_lastcallTS = Math.floor(new Date());
         console.log(output)
         stream.destroy()
+        /*
         if (output && '_text' in output && output._text.length)
             return output._text
         if (output && 'text' in output && output.text.length)
             return output.text
+        */
         return output;
     } catch (e) { console.log('transcribe_witai 851:' + e); console.log(e) }
 }
